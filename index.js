3b254bbcd3f0fa7c27991ed429b786fc99995213
varyakiryanova1 <kiryanova.varya@list.ru>
2021-12-28 17:07:17 +0000

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {pool} = require('./pgConfig')
+const PORT = process.env.PORT || 8080
+const app = express()
+
+app.get('/', (req, res) =>
+{
+    const sql1 = `select * from задания;`
+
+    pool.query(sql1, (err, resp) =>
+    {
+        if(err)
+            console.log('Error:', err)
+        else
+        {
+            res.render('problemset.ejs', {rows: resp.rows})
+        }
+    })
+
+})
+
+app.listen(PORT, ()=>
+    console.log('Приложение слушает порт:', PORT)
+    )
